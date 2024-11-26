cd684593f79aa0a0158d93d3fba137645e9aecf8
ben yeh <svj4hk@virginia.edu>
2024-11-26 02:37:44 -0500

    add function to get df from mongodb

--- ETL_pipeline/access_cloud.py
+++ ETL_pipeline/access_cloud.py
@@ -25,3 +25,27 @@ def store_data_to_cloud(uri, filepath, db_name, collection_name):
     result = collection.insert_many(data)    
     
     logger.info(f"Inserted {len(result.inserted_ids)} documents into {collection_name} collection!")
+
+def get_dataframe_from_cloud(uri, db_name, collection_name):
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        logger.info("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        logger.error(e)
+    
+    db = client[db_name]
+    collection = db[collection_name]
+
+    # Retrieve data from MongoDB collection
+    cursor = collection.find()
+    # Convert the cursor to a list of dictionaries (MongoDB documents)
+    data = list(cursor)
+    # Convert the list of dictionaries to a Pandas DataFrame
+    df = pd.DataFrame(data)
+
+    df.drop(columns=['_id'], inplace=True)
+
+    return df
